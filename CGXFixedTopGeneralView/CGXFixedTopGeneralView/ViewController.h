748a33d1b91e11480e1fcae0cd4d0ab23f1c5c81
974794055 <974794055@qq.com>
2018-08-18 13:33:45 +0800

    first

---
+++ CGXFixedTopGeneralView/CGXFixedTopGeneralView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CGXFixedTopGeneralView
+//
+//  Created by 曹贵鑫 on 2018/8/18.
+//  Copyright © 2018年 曹贵鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CGXFixedTopGeneralView/CGXFixedTopGeneralView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CGXFixedTopGeneralView
+//
+//  Created by 曹贵鑫 on 2018/8/18.
+//  Copyright © 2018年 曹贵鑫. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
