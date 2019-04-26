97922f2208dc8238774ff23fe4e40dec9b724e49
974794055 <974794055@qq.com>
2019-04-26 17:18:35 +0800

    first

--- CGXFixedTopGeneralView/CGXFixedTopGeneralView/CGXFixedTopGeneralView/Framework/CGXFixedTopBadgeLabel.h
+++ CGXFixedTopGeneralView/CGXFixedTopGeneralView/CGXFixedTopGeneralView/Framework/CGXFixedTopBadgeLabel.h
@@ -2,9 +2,9 @@
 //  CGXFixedTopBadgeLabel.h
 //  CGXFixedTopGeneralView
 //
-//  Created by 曹贵鑫 on 2018/8/8.
+//  Created by CGX on 2018/8/8.
 //  git下载链接：https://github.com/974794055/CGXFixedTopGeneralView-OC.git
-//  Copyright © 2018年 曹贵鑫. All rights reserved.
+//  Copyright © 2018年 CGX. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- CGXFixedTopGeneralView/CGXFixedTopGeneralView/PagtViewController.m
+++ CGXFixedTopGeneralView/CGXFixedTopGeneralView/PagtViewController.m
@@ -2,9 +2,9 @@
 //  PagtViewController.m
 //  CGXFixedTopGeneralView
 //
-//  Created by 曹贵鑫 on 2018/9/15.
+//  Created by CGX on 2018/9/15.
 //  git下载链接：https://github.com/974794055/CGXFixedTopGeneralView-OC.git
-//  Copyright © 2018年 曹贵鑫. All rights reserved.
+//  Copyright © 2018年 CGX. All rights reserved.
 //
 
 #import "PagtViewController.h"
