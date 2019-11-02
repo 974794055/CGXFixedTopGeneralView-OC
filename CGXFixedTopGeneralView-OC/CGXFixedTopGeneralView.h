6d360108885b672b650c10002bb67e1741e50441
974794055 <974794055@qq.com>
2019-11-02 18:54:57 +0800

    0.4

--- CGXFixedTopGeneralView-OC/CGXFixedTopGeneralView.h
+++ CGXFixedTopGeneralView-OC/CGXFixedTopGeneralView.h
@@ -5,6 +5,15 @@
 //  Created by MacMini-1 on 2019/5/22.
 //  Copyright © 2019 曹贵鑫. All rights reserved.
 //
+/*
+
+下载链接：https://github.com/974794055/CGXFixedTopGeneralView-OC.git
+QQ号：974794055
+群名称：
+CGXCategoryView交流群
+群   号：
+版本： 0.4
+*/
 
 #ifndef CGXFixedTopGeneralView_h
 #define CGXFixedTopGeneralView_h
